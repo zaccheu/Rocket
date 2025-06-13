3483a40d43dbff41f2a2c424586bd1c2fcf06171
Carolina Pavan <116219003+PavanCarol@users.noreply.github.com>
2025-06-13 13:21:34 -0300

    --route

--- NodeJS/Aulas/servers-http/src/server.js
+++ NodeJS/Aulas/servers-http/src/server.js
@@ -7,9 +7,6 @@
 // const http = require('http')
 //ESModules = >import/export
 import http from 'node:http'
-import {randomUUID} from 'node:crypto'
-import { json } from './middlewares/json.js'
-import { Database } from './database.js'
 
 //UUID => Universal Unique ID (ID unico universal)
 
@@ -24,27 +21,13 @@ const server = http.createServer(async (req,res)=>{
 
     await json(req, res)
 
-    if (method == 'GET' && url == '/users'){
-        const users = database.select('users')
-
-        return res.end(JSON.stringify(users))
-    }
-
-    if(method == 'POST' && url == '/users'){
-        const {name, email} = req.body
-        //escreve no banco de dados
-        // users.push({
-        //     id:1,
-        //     name,
-        //     email
-        // })
-        const user = {
-            id:randomUUID(),
-            name,
-            email
-        } 
-        database.insert('users', user)
-        return res.writeHead(201).end()
+   const route = routes.find(route =>{
+    return route.method == method 
+    &&
+    route.path == url 
+   })
+    if(route){
+        return route.handler(req,res)
     }
     return res.writeHead(404).end()
 })
